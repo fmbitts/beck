e39bddb75dacb4336e0333a985e9b3e68b4c2fc2
fmbitts <fmbitts@gmail.com>
2009-09-09 15:33:34 -0300

    Mudanca no calculo das resistencia, nao esta mais produzindo energia

--- beck.rb
+++ beck.rb
@@ -10,9 +10,9 @@ class Calcula_modelo
  
 	def initialize
 #			@ferro = {'modulo_criterio_estabilidade' => 4, 'difusividade_termica' => 5.87e-6, 'condutividade_termica' => 30, 'calor_especifico' => 700, 'densidade' => 7300}			
-			@aluminio = {'condutividade_termica_solido' => 213.0,'condutividade_termica_liquido' => 91.0, 'calor_especifico_solido' => 1181.0,'calor_especifico_liquido' => 1086.0, 'densidade_solido' => 2550.0,'densidade_liquido' => 2368.0}
+			@aluminio = {'condutividade_termica_solido' => 213.0,'condutividade_termica_liquido' => 91.0, 'calor_especifico_solido' => 1181.0,'calor_especifico_liquido' => 1086.0, 'densidade_solido' => 2550.0,'densidade_liquido' => 2368.0, 'temperatura_solidus' => 548, 'temperatura_liquidus ' => 645, 'coeficiente_particao' => 0.17, 'temperatura_fusao' => 660}
 #			puts "tempo de simulação em segundos"
-			@t_simulacao = 2 #gets.to_i
+			@t_simulacao = 200 #gets.to_i
 #			puts "Insira a temperatura ambiente"
 			t_ambiente =  30 #gets.to_i
 #			puts "Insira a temperatura do molde"
@@ -24,8 +24,8 @@ class Calcula_modelo
 	#		puts "insira a largura do metal"
 			@l_metal = 0.05 #gets.to_f
 	#		puts "inisira o numero de nós"
-			@n_nos_molde = 5
-			@n_nos_metal = 5
+			@n_nos_molde = 10
+			@n_nos_metal = 10
 			@n_nos =  @n_nos_molde + @n_nos_metal
 			puts "N nos:  " + @n_nos.to_s
 	# dividir nos por metal e molde(metade)
@@ -91,7 +91,7 @@ class Calcula_modelo
 #		calcula_delta_x(@l_molde,@l_metal,@n_nos)
 		alfa = calcula_difusividade('s')
 		delta_tempo = (@delta_x * @delta_x) / (2 * alfa)
-		@delta_tempo = delta_tempo * 0.01
+		@delta_tempo = delta_tempo * 0.001
 		puts "delta tempo:  " +  @delta_tempo.to_s
 		
 	return 	
@@ -109,101 +109,59 @@ class Calcula_modelo
 			cap = @aluminio['calor_especifico_liquido'] * @aluminio['densidade_liquido'] * @delta_x * @area
 		elsif estado == 's'
 			cap = @aluminio['calor_especifico_solido'] * @aluminio['densidade_solido'] * @delta_x * @area
+#			puts cap.to_s + "   capacitanica"
 		end
 		return cap
 	end
 
-	def calcula_R_anterior(estado)
+	def calcula_R(estado, *tipo)
 		if estado == 'l'
-			r_ant = (@delta_x) / (2 * @aluminio['condutividade_termica_liquido'] * @area)
+			r = (@delta_x) / (2 * @aluminio['condutividade_termica_liquido'] * @area)
 		elsif estado == 's'
-			r_ant = (@delta_x) / (2 * @aluminio['condutividade_termica_solido'] * @area)
+			r = (@delta_x) / (2 * @aluminio['condutividade_termica_solido'] * @area)
+#			puts "R:  " + r.to_s
 		end
-		return r_ant
+		return r
 	end
 	
-	def calcula_R_atual(estado)
-		if estado == 'l'
-			r_atual = (@delta_x) / (2 * @aluminio['condutividade_termica_liquido'] * @area)
-		elsif estado == 's'
-			r_atual = (@delta_x) / (2 * @aluminio['condutividade_termica_solido'] * @area)
-		end
-		return r_atual
-	end
-	
-	def calcula_R_proximo(estado)
-		if estado == 'l'
-			r_pro = (@delta_x) / (2 * @aluminio['condutividade_termica_liquido'] * @area)
-		elsif estado == 's'
-			r_pro = (@delta_x) / (2 * @aluminio['condutividade_termica_solido'] * @area)
-		end
-		return r_pro
-	end
 	
 	def calcula_R_hi
 		r_hi = 1 / (@hi * @area)
 	end
 	
 
-	def calcula_tal_carga(estado)
-		if estado == 'l'
-			capacitancia = calcula_cap_termica_no('l')
-			r_ant = calcula_R_anterior('l')
-			r_proximo = calcula_R_proximo('l')
-			r_atual = calcula_R_atual('l')
-		elsif estado == 's'	
-			capacitancia = calcula_cap_termica_no('s')
-			r_ant = calcula_R_anterior('s')
-			r_proximo = calcula_R_proximo('s')
-			r_atual = calcula_R_atual('s')
+	def calcula_tal(tipo,estado)
+		capacitancia = calcula_cap_termica_no(estado)
+		r_ant = calcula_R(estado)
+		r_proximo = calcula_R(estado)
+		r_atual = calcula_R(estado)
+		if tipo == 'carga'
+			tal = capacitancia * (r_ant +  r_atual)
+#			puts "tal:  " + tal.to_s
+		elsif tipo == 'descarga' 
+			tal = capacitancia * (r_atual + r_proximo)			
+		elsif tipo == 'resultante'
+			t_carga = calcula_tal('carga',estado)
+			t_descarga  = calcula_tal('descarga',estado)
+			tal = 1 / ((1 / t_carga) + (1 / t_descarga)) 
 		end
-		tal_carga = capacitancia * (r_ant + (2 * r_atual) + r_proximo)
-		return tal_carga
+		return tal
 	end
 
-		def calcula_tal_descarga(estado)
-			if estado == 'l'
-				capacidade = calcula_cap_termica_no('l')
-				r_ant = calcula_R_anterior('l')
-				r_proximo = calcula_R_proximo('l')
-				r_atual = calcula_R_atual('l')
-			elsif estado == 's'	
-				capacidade = calcula_cap_termica_no('s')
-				r_ant = calcula_R_anterior('s')
-				r_proximo = calcula_R_proximo('s')
-				r_atual = calcula_R_atual('s')
-			end
-			tal_descarga = capacidade * (r_ant + (2 * r_atual) + r_proximo)
-			return tal_descarga
-		end
-		
-			
-		def calcula_tal_resultante(estado)
-			if estado == 'l'
-				t_carga = calcula_tal_carga('l')
-				t_descarga  = calcula_tal_descarga('l')
-			elsif estado == 's'
-				t_carga = calcula_tal_carga('l')
-				t_descarga  = calcula_tal_descarga('l')
-			end		
-			tal_resul = (t_carga + t_descarga) / (t_carga * t_descarga)
-			return tal_resul
-		end
-			
-	
+	# Este metodo calcula a temperatura de cada no. Parametros :ciclo - ciclo de tempo :no em qual no :estados estado fisico do metal => liquido, solido ou mush
 	def calcula_temp(ciclo,no,pos,estado)
 		if pos == -1
-			temp = (@vet_temp[ciclo][no-1] * @delta_tempo) / (calcula_tal_descarga(estado))
+			temp = (@vet_temp[ciclo][no-1] * @delta_tempo) / (calcula_tal('descarga',estado))
 		elsif pos == 0
-			temp =  (1 - (@delta_tempo / calcula_tal_resultante(estado))) * @vet_temp[ciclo][no]
+			temp =  (1 - (@delta_tempo / calcula_tal('resultante',estado))) * @vet_temp[ciclo][no]
 		elsif pos == 1
-			temp = ((@vet_temp[ciclo][no+1] * (@delta_tempo)) / (calcula_tal_carga(estado))) 
+			temp = ((@vet_temp[ciclo][no+1] * (@delta_tempo)) / (calcula_tal('carga',estado))) 
 		end
 		return temp
 	end
 		
 
-	
+	# Este metodo calcula a temperatura final de cada no somando as parcelas dos nos anterior, autal e posterior
 	def temp_final
 		i = 0
 		n = 0
@@ -217,7 +175,11 @@ class Calcula_modelo
 						@vet_temp[n+1][i] = calcula_temp(n,i,(-1),'s') + calcula_temp(n,i,0,'s') + calcula_temp(n,i,1,'s')
 						puts "Ciclo: " + (n+1).to_s + "  Nos:  " + i.to_s + "  Temperatura :  "  + @vet_temp[n+1][i].to_s						
 					elsif i > @n_nos_molde # calculo da temperatura para estado liquido METAL
-						@vet_temp[n+1][i] = calcula_temp(n,i,-1,'l') + calcula_temp(n,i,0,'l') + calcula_temp(n,i,1,'l')
+						if @vet_temp[n][i] <= @aluminio['temperatura_solidus']
+							@vet_temp[n+1][i] = calcula_temp(n,i,-1,'s') + calcula_temp(n,i,0,'s') + calcula_temp(n,i,1,'s')
+						else	
+							@vet_temp[n+1][i] = calcula_temp(n,i,-1,'l') + calcula_temp(n,i,0,'l') + calcula_temp(n,i,1,'l')
+						end
 						puts "Ciclo: " + (n+1).to_s + "  Nos:  " + i.to_s + "  Temperatura :  " + @vet_temp[n+1][i].to_s
 					end
 				elsif i == (@n_nos_molde - 1) # calculo da temperatura interface
