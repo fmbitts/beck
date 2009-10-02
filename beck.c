5b7f40b0255e45f387f2d1c6606c8cbd338f4f70
Filipi Vianna <filipi@brainstorm.(none)>
2009-10-02 20:42:57 -0300

    Creating a structure to store metal properties.

--- beck.c
+++ beck.c
@@ -2,59 +2,41 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define CHECKER_VERSION "$Id: beck.c,v 1.12 2009/09/09 21:31:42 filipi Exp $"
+#define BECK_VERSION "0.0.1"
 #define	EXIT_FAILURE  1
 #define	EXIT_SUCCESS  0
-//Metal properties
-static float Ks=213;  // condutividade termica do solido
-static float Kl=91;   // condutividade termica do liquido
-static float Cs=1181; // calor especifico solido
-static float Cl=1086; //calor especifico liquido
-static float Ds=2550; // densidade solido
-static float Dl=2368; //densidade loquido
-static float Ts=548;  //temperatura solidus
-static float Tl=645;  //temperatura liquidus
-static float Cp=0.17; //coeficiente de particao
-static float TF=660;  //temperatura fusao
 
-//
-struct tmap {
-  int col, line, val, top, bottom;
-  unsigned char busy;
-  } *map;
-
-unsigned char locateVal(int value, int count, int *col, int *line, int *index,\
-                        struct tmap *map);
-unsigned char locateXY(int col, int line, int count, int *val, int *index, \
-                       struct tmap *map);
-int maxDepth(struct tmap *map, int count);
+struct sproperties {
+  float Ks,  // Solid thermal conductivity
+        Kl,  // Liquid thermal conductivity
+        Cs,  // Solid specific heat capacity
+        Cl,  // Liquid specific heat capacity
+        Ds,  // Solid density
+        Dl,  // Liquid density
+        Pc,  // Partition coefficient
+        Ts,  // Solid temperature
+        Tl,  // Liquid temperature
+        Tm;  // Melting temperature
+  };
 
 int quiet;
-//
 
 void usage(void);
 void printversion(void);
 void flush_in();
 void *mallocX (unsigned int nbytes);
 void *reallocX (void *ptr, unsigned int nbytes);
-void frac_part(float *fs, float *dfs,float temp);
-void properties(float temp);
 
+void getFracPart(float *fs, float *dfs, float temp, \
+                 struct sproperties properties);
+void showProperties(float temp, struct sproperties *properties);
 
 int main(int argc, char *argv[]){
-  int col = 0, line = 0;
-  int lastLine = 0, lastColumn = 0, *index;
-  char *arg, disableEqualTest = 0;
-
-  int count, i,left, right, *vecleft, *vecright, val;
-  int j, k;
-  char oper, *vecoper;
-
-  ///////////
   FILE *propsFile;
-  int metalNodes = 0, moldNodes = 0, nodes = 0;
+  int metalNodes = 0, moldNodes = 0, nodes = 0, i;
   float *vecTemp;
-  char *filename = 0;
+  char *filename = 0, *arg;
+  struct sproperties properties;
 
   // Parse Command Line
   for (i=1; i<argc; i++) {
@@ -64,37 +46,37 @@ int main(int argc, char *argv[]){
       switch (arg[1]) {
       case 'm':
         if (argc > (i+1)) {
-  	      metalNodes = atoi(argv[i+1]);
-	      i++;
+  	  metalNodes = atoi(argv[i+1]);
+	  i++;
         }
       break;
       case 'M':
         if (argc > (i+1)) {
-  	      moldNodes = atoi(argv[i+1]);
-	      i++;
+  	  moldNodes = atoi(argv[i+1]);
+	  i++;
         }
       break;
       case 'f':
         if (argc > (i+1)) {
-  	      filename = argv[i+1];
-	      i++;
+  	  filename = argv[i+1];
+	  i++;
         }
       break;
       case 'h':
-	    usage();
-	    exit(EXIT_SUCCESS);
-	  break;
+        usage();
+	exit(EXIT_SUCCESS);
+      break;
       case 'q' :
         quiet = 1;
       break;
       case 'v' :
-	    printversion();
-	    exit(EXIT_SUCCESS);
-	  break;
+        printversion();
+	exit(EXIT_SUCCESS);
+      break;
       default:
         usage();
-	    exit(EXIT_SUCCESS);
-	  break;
+	exit(EXIT_SUCCESS);
+      break;
       }
     }
   }
@@ -130,11 +112,22 @@ int main(int argc, char *argv[]){
   for (i=0;i<nodes;i++){
     if (!quiet) printf("Temperature on node %d: %1.2f\n", i, vecTemp[i]);
   }
+  properties.Ks = 213;  // Solid thermal conductivity
+  properties.Kl = 91;   // Liquid thermal conductivity
+  properties.Cs = 1181; // Solid specific heat capacity
+  properties.Cl = 1086; // Liquid specific heat capacity
+  properties.Ds = 2550; // Solid density
+  properties.Dl = 2368; // Liquid density
+  properties.Ts = 548;  // Solid temperature
+  properties.Tl = 645;  // Liquid temperature
+  properties.Pc = 0.17; // Partition coefficient
+  properties.Tm = 660;  // Melting temperature
   if (!quiet){
-    printf("Tf:  %1.4f \n",TF);	
-    printf("Tl:  %1.4f \n",Tl);
+    printf("Melting temperature:  %1.4f \n", properties.Tm);	
+    printf("Liquid temperature:   %1.4f \n", properties.Tl);
   }
-  properties(600);
+
+  showProperties(600, &properties);
   free(vecTemp);
 
   if (!quiet) printf("\nBeck finished successfully!!\n");
@@ -146,48 +139,28 @@ int main(int argc, char *argv[]){
   return EXIT_SUCCESS;
 }
 
-unsigned char locateVal(int value, int count, int *col, int *line, \
-                        int *index, struct tmap *map){
-  int i = 0,  j = 0;
-  unsigned char success = 0;
-  while( (i<count) && (success==0) ){
-    if (map[i].val == value){
-      *col = map[i].col;
-      *line = map[i].line;
-      success = 1;
-      j = i;
-    }
-    i++;
-  }
-  *index = j;
-  return success;
+void getFracPart(float *fs, float *dfs,float temp, \
+                 struct sproperties properties){
+  float a, b, c;
+  a = ( (properties.Tm - temp) / (properties.Tm - properties.Tl) );
+  if (!quiet) printf("a:  %1.4f \n", a);
+  b = 1 / (properties.Pc -1);
+  if (!quiet) printf("b: %1.4f \n", b);
+  c = 1 /(properties.Tm - properties.Tl);
+  if (!quiet) printf("c: %1.4f \n", c);
+  *fs = 1 - ( powf(a,b) );
+  *dfs = powf(a,b);
 }
 
-unsigned char locateXY(int col, int line, int count, int *val, \
-                       int *index, struct tmap *map){
-  int i = 0, j = 0;
-  unsigned char success = 0;
-  while( (i<count) && (success==0) ){
-    if ( (map[i].col == col) && (map[i].line == line && (map[i].val != 0) ) ){
-      *val = map[i].val;
-      j = i;
-      success = 1;
-    }
-    i++;
+void showProperties(float temp, struct sproperties *properties){
+  float fs = 0;
+  float dfs = 0;
+  getFracPart(&fs, &dfs, temp, *properties);
+  if (!quiet){
+    printf("temp: %1.4f \n",temp);	
+    printf("fs:  %1.4f \n",fs);
+    printf("dfs: %1.4f  \n",dfs);
   }
-  *index = j;
-  return success;
-}
-
-int maxDepth(struct tmap *map, int count){
-  int i = 0, iniDepth = 0;
-  while( (i<2*count) && (map[i].val!=0) ){
-    if (map[i].line > iniDepth){ 
-      iniDepth = map[i].line;
-    }
-    i++;
-  }
-  return iniDepth;
 }
 
 void usage(void) {
@@ -206,7 +179,7 @@ void usage(void) {
 }
 
 void printversion(void) {
-  fprintf(stderr, "%s\n", CHECKER_VERSION);
+  fprintf(stderr, "%s\n", BECK_VERSION);
 }
 
 void flush_in(){
@@ -218,7 +191,7 @@ void *mallocX (unsigned int nbytes){
   void *ptr;
   ptr = malloc (nbytes);
   if (ptr == NULL) {
-    printf ("Error allocating memory - malloc returned NULL!\n");
+    fprintf (stderr, "Error allocating memory - malloc returned NULL!\n");
 #ifdef WIN32
     system("pause");
 #endif
@@ -230,7 +203,7 @@ void *mallocX (unsigned int nbytes){
 void *reallocX (void *ptr, unsigned int nbytes){
   ptr = realloc (ptr, nbytes);
   if (ptr == NULL) {
-    printf ("Error reallocating memory - realloc returned NULL!\n");
+    fprintf (stderr, "Error reallocating memory - realloc returned NULL!\n");
 #ifdef WIN32
     system("pause");
 #endif
@@ -238,28 +211,3 @@ void *reallocX (void *ptr, unsigned int nbytes){
   }
   return ptr;
 }
-
-void frac_part(float *fs, float *dfs,float temp){	
-  float a, b, c;
-  a = ((TF - temp) / (TF -Tl));
-  if (!quiet) printf("a:  %1.4f \n", a);	
-  b = 1 / (Cp -1);
-  if (!quiet) printf("b: %1.4f \n", b);
-  c = 1 /(TF - Tl);
-  if (!quiet) printf("c: %1.4f \n", c);
-  *fs = 1 - (powf(a,b));
-  *dfs = powf(a,b);
-}
-
-void properties(float temp){
-  float fs = 0;
-  float dfs = 0;
-  frac_part(&fs, &dfs, temp);
-  if (!quiet){
-    printf("temp: %1.4f \n",temp);	
-    printf("fs:  %1.4f \n",fs);
-    printf("dfs: %1.4f  \n",dfs);
-  }
-}
-
-	
